37ee5d7fd28fbe8a3c145996a0c5177eabaedcad
bumbelbee777 <64847808+bumbelbee777@users.noreply.github.com>
2022-07-10 19:53:37 +0100

    remove unused header

--- src/cxxrt/cxxrt.cpp
+++ src/cxxrt/cxxrt.cpp
@@ -1,6 +1,5 @@
-#include <core/panic.hpp>
 #include <cxx/cstdint>
 
 extern "C" int __cxa_atexit([[maybe_unused]] void (*func)(void *), [[maybe_unused]] void *arg, [[maybe_unused]] void *dso_handle) {
-        return 0;
-    }
+    return 0;
+}
