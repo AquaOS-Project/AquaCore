f9329f0a882b90291deaf07973e11aba5d8f0d09
bumbelbee777 <64847808+bumbelbee777@users.noreply.github.com>
2022-07-10 19:42:58 +0100

    really simple c++ excpetions handler

---
+++ src/cxxrt/cxxrt.cpp
@@ -0,0 +1,12 @@
+#include <core/panic.hpp>
+#include <cxx/cstdint>
+
+extern "C" {
+    int __cxa_atexit([[maybe_unused]] void (*func)(void *), [[maybe_unused]] void *arg, [[maybe_unused]] void *dso_handle) {
+        return 0;
+    }
+
+    void __cxa_pure_virtual() {
+        panic("Pure virtual function called!");
+    }
+}
